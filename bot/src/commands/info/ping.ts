a4cad2059205e759e77a34a79807db785ec7a816
PyxelCodes <pyxeldev@icloud.com>
2024-08-05 20:38:12 +0200

    /ping

--- bot/src/commands/info/ping.ts
+++ bot/src/commands/info/ping.ts
@@ -1,7 +1,22 @@
-// export default {
-//     name: 'ping',
-//     description: 'Ping!',
-//     async run({ interaction }) {
+import { Command, MessageEmbed } from "../../aetherial/src"
+import { ShardInteraction } from "../../aetherial/src/sharding/ShardInteraction";
 
-//     }
-// }
+export default {
+    name: 'ping',
+    description: 'Ping!',
+    async run({ interaction }) {
+        if(interaction instanceof ShardInteraction !== true) return; // Typed it wrong L
+
+        let ldEmbed = new MessageEmbed().setColor(0x2f3136).setDescription('Pinging... <a:Loading:1270072089730093118>');
+        interaction.reply({ embeds: [ldEmbed] }).then((msg) => {
+            interaction.editReply({
+                embeds: [
+                    new MessageEmbed()
+                        .setColor(0x2f3136)
+                        .addField('Latency', `${msg.createdTimestamp - interaction.createdTimestamp}ms`)
+                        .addField('API Latency', `${Math.round(interaction.client.ping)}ms`)
+                ]
+            })
+        });
+    }
+} as Command<ShardInteraction>;
